65d4144e57d12559e3713c4224be0a5df995341d
cfschilham <cfschilham@gmail.com>
2020-02-03 21:15:51 +0100

    Move installers to separate repo

--- scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ `command -v python` != "" ]; then
-    sudo python -c "$(curl https://raw.githubusercontent.com/cfschilham/kryer/master/scripts/install.py -s)"
-elif [ `command -v python3` != "" ]; then
-    sudo python3 -c "$(curl https://raw.githubusercontent.com/cfschilham/kryer/master/scripts/install.py -s)"
-else
-    echo "Please install any version of python"
-fi
