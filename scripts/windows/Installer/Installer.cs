0ac49ff59c406be204f36c4d4c421d39e7f62297
OsOmE1 <ben.stokmans@clz.nl>
2020-02-03 17:34:30 +0100

    Added installer files

---
+++ scripts/windows/Installer/Installer.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Installer
+{
+    class Installer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
